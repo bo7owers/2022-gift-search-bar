e991227857b86731bf0749f671966682eb0b2275
bo7owers <7orres.rene@proton.me>
2022-12-01 19:47:37 -0500

    Add imgs to res & call json as response

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,17 @@
 import { h, ref, watch } from 'vue'
 import debounce from 'debounce'
 
-const searchTerm = ref(' ')
-const isLoading = ref(false)
 const productArray = ref([])
+const isLoading = ref(false)
+const searchTerm = ref(' ')
 
 const findProducts = debounce(async term => {
   try {
     if (term === 0) return (productArray.value = [])
     isLoading.value = true
-    const findTerms = await fetch(`https://dummyjson.com/products/search?q=${term}&limit=10`)
-    productArray.value = findTerms.productArray
-    console.table(productArray.value)
+    const findTerms = await await (await fetch(`https://dummyjson.com/products/search?q=${term}&limit=10`)).json()
+    productArray.value = findTerms.products
+    console.table(findTerms)
   } catch (error) {
     console.error(error)
   } finally {
@@ -24,12 +24,19 @@ watch(searchTerm, newTerm => findProducts(newTerm))
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col gap-5 justify-center items-center">
+  <div class="w-full flex flex-col gap-5 justify-center items-center">
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
-    <ul class="list-disc">
-      <li v-for="product in productArray" :key="product.id">{{ product.price }} {{ product.title }}</li>
+    <ul class="list-disc" v-if="!isLoading">
+      <li v-for="product in productArray" :key="product.id">
+        {{ product.title }} ${{ product.price }}
+        <img
+          :src="product.thumbnail"
+          class="bg-white border-solid border-2 border-sky-500 rounded max-w-xs object-cover"
+          :alt="product.description"
+        />
+      </li>
     </ul>
-    <div class="loading" v-if="isLoading">Loading...</div>
+    <div class="loading" v-else-if="isLoading">Loading...</div>
   </div>
 </template>

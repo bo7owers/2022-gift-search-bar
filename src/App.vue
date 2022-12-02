96cd9fe06125ee8bf8b6b1f57d3a96762ccd3de4
bo7owers <7orres.rene@proton.me>
2022-12-01 19:28:22 -0500

    Install debounce

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { h, ref, watch } from 'vue'
+import debounce from 'debounce'
 
-const searchTerm = ref('')
+const searchTerm = ref(' ')
+const isLoading = ref(false)
+const productArray = ref([])
 
-const findProducts = async term => {}
+const findProducts = debounce(async term => {
+  try {
+    if (term === 0) return (productArray.value = [])
+    isLoading.value = true
+    const findTerms = await fetch(`https://dummyjson.com/products/search?q=${term}&limit=10`)
+    productArray.value = findTerms.productArray
+    console.table(productArray.value)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isLoading.value = false
+  }
+}, 300)
 
 watch(searchTerm, newTerm => findProducts(newTerm))
 </script>
@@ -13,7 +28,8 @@ watch(searchTerm, newTerm => findProducts(newTerm))
     <h1 class="text-4xl font-bold">Gift Search Bar</h1>
     <input type="text" class="p-2 border-2 border-gray-dark" v-model="searchTerm" placeholder="Start typing..." />
     <ul class="list-disc">
-      <li>Display suggestions here</li>
+      <li v-for="product in productArray" :key="product.id">{{ product.price }} {{ product.title }}</li>
     </ul>
+    <div class="loading" v-if="isLoading">Loading...</div>
   </div>
 </template>
